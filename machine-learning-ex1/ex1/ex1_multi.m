f1165dadadc64c9bb12142311b7f1abce0f8da2f
Joao-M-Almeida <jmirandadealme@gmail.com>
2015-11-19 23:12:22 +0000

    Finished first assignment

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -3,11 +3,11 @@
 %
 %  Instructions
 %  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear regression exercise. 
 %
-%  You will need to complete the following functions in this 
+%  This file contains code that helps you get started on the
+%  linear regression exercise.
+%
+%  You will need to complete the following functions in this
 %  exericse:
 %
 %     warmUpExercise.m
@@ -51,6 +51,11 @@ fprintf('Normalizing Features ...\n');
 
 [X mu sigma] = featureNormalize(X);
 
+fprintf('First 10 examples from the dataset: \n');
+fprintf(' x = [%.0f %.0f], y = %.0f \n', [X(1:10,:) y(1:10,:)]');
+pause;
+
+
 % Add intercept term to X
 X = [ones(m, 1) X];
 
@@ -60,13 +65,13 @@ X = [ones(m, 1) X];
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
 %               code that runs gradient descent with a particular
-%               learning rate (alpha). 
+%               learning rate (alpha).
 %
-%               Your task is to first make sure that your functions - 
-%               computeCost and gradientDescent already work with 
+%               Your task is to first make sure that your functions -
+%               computeCost and gradientDescent already work with
 %               this starter code and support multiple variables.
 %
-%               After that, try running gradient descent with 
+%               After that, try running gradient descent with
 %               different values of alpha and see which one gives
 %               you the best result.
 %
@@ -85,7 +90,7 @@ fprintf('Running gradient descent ...\n');
 alpha = 0.01;
 num_iters = 400;
 
-% Init Theta and Run Gradient Descent 
+% Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
 [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
 
@@ -120,12 +125,12 @@ pause;
 fprintf('Solving with normal equations...\n');
 
 % ====================== YOUR CODE HERE ======================
-% Instructions: The following code computes the closed form 
+% Instructions: The following code computes the closed form
 %               solution for linear regression using the normal
-%               equations. You should complete the code in 
+%               equations. You should complete the code in
 %               normalEqn.m
 %
-%               After doing so, you should complete this code 
+%               After doing so, you should complete this code
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
 
@@ -156,4 +161,3 @@ price = 0; % You should change this
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using normal equations):\n $%f\n'], price);
-
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,13 +11,15 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
 
+    J_derivative = 1/m * sum((X*theta-y).*X,1);
+    theta = theta - alpha * J_derivative';
 
 
 
@@ -29,7 +31,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
